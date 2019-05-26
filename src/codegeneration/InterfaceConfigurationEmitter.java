e505d18887c40eda183dd95dc01f71caa82bd935
nbakga14 <nbakga14@student.aau.dk>
2019-05-26 22:51:27 +0200

    Implemented equals og hashcode on InterfaceConfigurationEmitter
    Implemented equals og hashcode on InterfaceConfigurationEmitter

--- src/codegeneration/InterfaceConfigurationEmitter.java
+++ src/codegeneration/InterfaceConfigurationEmitter.java
@@ -7,6 +7,7 @@ import symbols.PhysicalInterface;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 public class InterfaceConfigurationEmitter extends ConfigurationEmitter{
@@ -33,5 +34,19 @@ public class InterfaceConfigurationEmitter extends ConfigurationEmitter{
             writer.write(String.format("Interface %s %d/%d", ifI.getInterfaceType().toString(), ifI.getIndex1(), ifI.getIndex2())+System.getProperty("line.separator"));
         }
         super.writeCommand(writer);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        InterfaceConfigurationEmitter that = (InterfaceConfigurationEmitter) o;
+        return Objects.equals(inf, that.inf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), inf);
     }
 }
